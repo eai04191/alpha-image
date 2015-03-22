1cbc5df2cbf9fa922666cc345c18c4e127e797d3
Eai <eai04191@gmail.com>
2015-03-23 05:14:41 +0900

    こめんと

--- index.php
+++ index.php
@@ -26,15 +26,18 @@ $html_before = <<< EOM
 </head>
 <body>
 <a href="https://github.com/eai04191/alpha_image"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"></a>
-  <h1>画像を透過するやつ</h1>
+  <h1>画像を透明にするやつ</h1>
 
   <section>
     <h1>使い方</h1>
     <p>{$ownURL}?src=<span class="sugoi">加工元画像URL</span>&opacity=<span class="sugoi">透明度</span></p>
     <h2>透明度について</h2>
     <p><span class="light">薄い</span> <span class="sugoi">0.0~1.0</span> <span class="dark">濃い</span></p>
+
+    <p>直リンで使ってもいいけどあんまり強いサーバーじゃないから落とさないように気を付けて。</p>
+    <p>ソースコード: <a href="https://github.com/eai04191/alpha_image">https://github.com/eai04191/alpha_image</a></p>
   </section>
-  
+
   <section>
     <h1>エラー</h1>
     <ul class="red"">
